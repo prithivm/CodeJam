7dec60a11bc7a73cd10085cf1b066d20d1038a7f
prithivm <prithiv@106.51.34-broadband.acttv.in>
2013-10-13 19:38:13 +0530

    Anther Solution Added

--- Moist/solution.py
+++ Moist/solution.py
@@ -21,5 +21,30 @@ for i in range(t):
             cnt += 1
     output="Case #"+str(i+1)+": "+str(cnt)+'\n'
     out.write(output)
+    
+    
+#ANOTHER SOLUTION
+
+
+def read_N_Lines(f,n):
+    lines = []
+    for x in range(0,n):
+        lines.append(f.readline())
+    return lines
+    
+f = open('CSmall','r')
+case = int(f.readline())
+while case > 0:
+    cnt = 0
+    num = int(f.readline())
+    items = read_N_Lines(f,num)
+    m = items[0]
+    for item in items:
+        if item >= m:
+            m = item
+        elif item < m:
+            cnt += 1
+    print cnt
+    case -= 1
              
     
